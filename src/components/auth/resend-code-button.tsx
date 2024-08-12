48bd115000110f31b6d9ff5d2a42f9b7b18fec25
Rashid Ameer <r.ameer.offical@gmail.com>
2024-08-12 14:29:39 +0500

    Change resend code button styling

--- src/components/auth/resend-code-button.tsx
+++ src/components/auth/resend-code-button.tsx
@@ -1,7 +1,7 @@
 import { resendEmailVerificationCode } from "@/actions/auth";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { useState, useTransition } from "react";
+import { useEffect, useState, useTransition } from "react";
 import toast from "react-hot-toast";
 import CircularLoader from "../circular-loader";
 
@@ -11,6 +11,7 @@ type ResendCodeButtonProps = {
 
 function ResendCodeButton({ className }: ResendCodeButtonProps) {
   const [isPending, startTransition] = useTransition();
+  const [countDown, setCountDown] = useState(0);
 
   // handle click
   const handleClick = () => {
@@ -19,6 +20,7 @@ function ResendCodeButton({ className }: ResendCodeButtonProps) {
         const result = await resendEmailVerificationCode();
         if (result.success) {
           toast.success("Code sent successfully");
+          setCountDown(30);
         }
       } catch (error) {
         toast.error("Failed to resend code");
@@ -26,16 +28,33 @@ function ResendCodeButton({ className }: ResendCodeButtonProps) {
     });
   };
 
+  useEffect(() => {
+    if (countDown === 0) return;
+
+    const timer = setTimeout(() => setCountDown(countDown - 1), 1000);
+    return () => clearTimeout(timer);
+  }, [countDown]);
+
   return (
-    <div className={cn("text-sm flex gap-2 items-center", className)}>
+    <div
+      className={cn(
+        "text-sm text-gray-500 flex gap-2 items-center",
+        className
+      )}>
       Didn&apos;t rececive code?{" "}
       <Button
         onClick={handleClick}
         type="button"
         variant="link"
         className="p-0 h-auto"
-        disabled={isPending}>
-        Resend Code
+        disabled={isPending || countDown > 0}>
+        {countDown > 0 ? (
+          <>
+            Resend in <span className="tabular-nums ml-1">{countDown}s</span>
+          </>
+        ) : (
+          "Resend Code"
+        )}
       </Button>
     </div>
   );
